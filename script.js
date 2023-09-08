0a741e5f1739a1b98e8feef5c9e890babd5f5f78
IT MVCP FR <115785416+itdclmfr@users.noreply.github.com>
2023-09-08 15:59:37 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const heuresElement = document.getElementById('heures');
+    const minutesElement = document.getElementById('minutes');
+    const secondesElement = document.getElementById('secondes');
+
+    const dateFin = new Date('2023-12-31T23:59:59'); // Définissez votre date de fin ici
+    const interval = setInterval(mettreAJourCompteur, 1000);
+
+    function mettreAJourCompteur() {
+        const maintenant = new Date();
+        const difference = dateFin - maintenant;
+
+        if (difference <= 0) {
+            clearInterval(interval);
+            heuresElement.textContent = '00';
+            minutesElement.textContent = '00';
+            secondesElement.textContent = '00';
+        } else {
+            const heures = Math.floor(difference / 3600000);
+            const minutes = Math.floor((difference % 3600000) / 60000);
+            const secondes = Math.floor((difference % 60000) / 1000);
+
+            heuresElement.textContent = heures.toString().padStart(2, '0');
+            minutesElement.textContent = minutes.toString().padStart(2, '0');
+            secondesElement.textContent = secondes.toString().padStart(2, '0');
+        }
+    }
+});
